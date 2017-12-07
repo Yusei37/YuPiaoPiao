726c8b3cb2a6a0f224f69408d4702b0b7f5349e0
Yusei37 <1140254803@qq.com>
2017-12-07 19:47:12 +0800

    UI changed

--- app/src/main/java/com/example/yusei/yupiaopiao/MainActivity.java
+++ app/src/main/java/com/example/yusei/yupiaopiao/MainActivity.java
@@ -3,15 +3,18 @@ package com.example.yusei.yupiaopiao;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
+import android.support.v7.widget.Toolbar;
+import android.view.*;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    private TextView topBar;
     private TextView tabDeal;
     private TextView tabPoi;
     private TextView tabMore;
@@ -23,22 +26,53 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private SecondFragment f1;
     private FragmentManager fragmentManager;
 
+    private DrawerLayout mDrawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
         bindView();
+//        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+//        if (actionBar != null) {
+//            actionBar.hide();
+//        }
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_face_black_18dp);
+        }
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
+    }
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+        }
+        return true;
     }
 
     //UI组件初始化与事件绑定
     private void bindView() {
-        topBar = (TextView)this.findViewById(R.id.txt_top);
         tabDeal = (TextView)this.findViewById(R.id.txt_deal);
         tabPoi = (TextView)this.findViewById(R.id.txt_poi);
         tabUser = (TextView)this.findViewById(R.id.txt_user);
